eaf09abeba3a6cba4f4a33b2240a703a7e03feee
Jahva1229 <elijah.smith122904@gmail.com>
2025-02-13 04:15:21 -0500

    Add initial implementation of graph input reading and adjacency list creation

---
+++ PA1/test.py
@@ -0,0 +1,43 @@
+import unittest
+from pa1 import create_adj_list
+
+class TestCreateAdjList(unittest.TestCase):
+    def test_empty(self):
+        E = []
+        V = []
+        expected = {}
+        self.assertEqual(create_adj_list(E, V), expected)
+
+    def test_one_edge(self):
+        E = [(0, 0, 0, 1, 1)]
+        V = [(0, 0), (0, 1)]
+        expected = {
+            (0, 0): [((0, 1), 1)], 
+            (0, 1): [((0,0), 1)]
+            }
+        self.assertEqual(create_adj_list(E, V), expected)
+    
+    def test_two_edges(self):
+        E = [(0, 0, 0, 1, 1), (0, 0, 1, 0, 1)]
+        V = [(0, 0), (0, 1), (1, 0)]
+        expected = {
+            (0, 0): [((0, 1), 1), ((1, 0), 1)], 
+            (0, 1): [((0,0), 1)],
+            (1, 0): [((0,0), 1)]
+        }
+        self.assertEqual(create_adj_list(E, V), expected)
+
+    def test_duplicate_edges(self):
+        E = [(0, 0, 0, 1, 1), (0, 0, 0, 1, 1)]
+        V = [(0, 0), (0, 1)]
+        expected = {
+            (0, 0): [((0, 1), 1)], 
+            (0, 1): [((0,0), 1)]
+        }
+        self.assertEqual(create_adj_list(E, V), expected)
+    
+
+
+
+if __name__ == '__main__':
+    unittest.main()
